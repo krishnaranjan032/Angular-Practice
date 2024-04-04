42da1360fcee1fbfbf20e82be2c8570af7684182
Krishna Ranjan <krishna.ranjan@mobiuso.com>
2024-04-04 14:52:19 +0530

    add list for recipes and create Ingredient model

--- src/app/recipes/recepe-list/recepe-list.component.ts
+++ src/app/recipes/recepe-list/recepe-list.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recepe-list',
   templateUrl: './recepe-list.component.html',
-  styleUrl: './recepe-list.component.css'
+  styleUrl: './recepe-list.component.css',
 })
 export class RecepeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test recipe',
+      'This is just done for testing purpose',
+      'https://media.istockphoto.com/id/1363638825/photo/vegan-plant-based-asian-food-recipes-with-rice-and-brown-rice-as.webp?b=1&s=170667a&w=0&k=20&c=EXreVl3ug6ng9updrHKAFagrNctbOCIPDpaziJ6rVXg='
+    ),
+    new Recipe(
+      'A Test recipe',
+      'This is just done for testing purpose',
+      'https://media.istockphoto.com/id/1363638825/photo/vegan-plant-based-asian-food-recipes-with-rice-and-brown-rice-as.webp?b=1&s=170667a&w=0&k=20&c=EXreVl3ug6ng9updrHKAFagrNctbOCIPDpaziJ6rVXg='
+    ),
+    new Recipe(
+      'A Test recipe',
+      'This is just done for testing purpose',
+      'https://media.istockphoto.com/id/1363638825/photo/vegan-plant-based-asian-food-recipes-with-rice-and-brown-rice-as.webp?b=1&s=170667a&w=0&k=20&c=EXreVl3ug6ng9updrHKAFagrNctbOCIPDpaziJ6rVXg='
+    ),
+  ];
+  constructor() {}
 }
