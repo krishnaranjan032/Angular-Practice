d4727f78a8f7c88bfd7ad72366786b43e1b84988
Krishna Ranjan <krishna.ranjan@mobiuso.com>
2024-04-04 15:28:26 +0530

    add list option for shopping item

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-  ingredients: []
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Apple', 5),
+    new Ingredient('Orange', 10),
+  ];
 }
